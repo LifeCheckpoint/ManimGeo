34fc2681fce7621ba81b32096f6b309672e75ca6
lifecheckpoint <lifechpt@qq.com>
2025-06-25 13:43:12 +0800

    multiple参数模型

---
+++ src/manimgeo/components/multiple/args.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from ..base import ArgsModelBase
+from typing import Union, Literal, List
+
+type Number = Union[float, int]
+
+from ..base import BaseGeometry
+
+class MultipleArgs(ArgsModelBase):
+    construct_type: Literal["Multiple"] = "Multiple"
+    geometry_objects: List[BaseGeometry]
